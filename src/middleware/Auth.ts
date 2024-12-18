307d1eefb7d25ebc5f454c4b87fee4f806cd2d32
Victor Ponfilov <victor.converto@gmail.com>
2024-12-18 14:30:22 +0200

    switch simple auth to JWT

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 import config from '../config';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-	const { code } = req.query;
-	if (code === config.AUTH_CODE) next();
-	else res.status(403).send();
+	const token = req.query.token as string;
+	try {
+		jwt.verify(token, config.AUTH_CODE);
+		next();
+	} catch (error) {
+		res.status(401).json({ error: 'Unauthorized' });
+	}
 };
 
 export default authMiddleware;
